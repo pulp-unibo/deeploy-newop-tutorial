3d1710f68496676fe0109e4f564aa2af7a8c7462
Francesco Conti <f.conti@unibo.it>
2025-06-05 09:25:15 +0000

    add missing kernel stubs

---
+++ kernel/PULP_vectorAdd.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2025 ETH Zurich and University of Bologna.
+ *
+ * Author: Francesco Conti <f.conti@unibo.it>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pmsis.h"
+
+void PULP_vectorAdd_u8_u8_u8(uint8_t *x, uint8_t *y, uint8_t *z,
+                  int size);
+void PULP_vectorAdd_s8_s8_u8(int8_t *x, int8_t *y, int8_t *z,
+                  int size);
